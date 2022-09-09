894caaa3c515bd39771f8803e75f8435bac4f0bc
blair chappell <blairhb7@yahoo.com>
2022-09-09 15:26:04 -0700

    done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import 'aos/dist/aos.css';
 import Link from 'next/link';
 import Nav from '../Components/Nav';
 import MobileNav from '../Components/MobileNav';
-import { motion} from 'framer-motion'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <AnimatePresence exitBeforeEnter>
