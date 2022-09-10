f0a3cc54ed07e742f8b317cdfa2964e59c8d5f34
blair chappell <blairhb7@yahoo.com>
2022-09-09 17:07:52 -0700

    done

--- pages/LandingPage.tsx
+++ pages/LandingPage.tsx
@@ -16,7 +16,7 @@ const LandingPage = () => {
     <motion.div exit={{ scaleY: 0}} initial={{ scaleY: 0}} animate={{ scaleY: 1}} transition={{duration:0.5}}  className='h-screen'>
       <Nav />
       <MobileNav />
-      <div className=" flex flex-col justify-center items-center h-screen landingpage">
+      <div className=" flex flex-col justify-center items-center h-[1000px] xl:h-screen landingpage">
         <h2  data-aos="fade" data-aos-offset="200" data-aos-delay="120" data-aos-duration="9000" className="uppercase flex font-cinzel text-lg  xl:text-2xl">Blair Chappell</h2>
         <h1 data-aos="fade-in" data-aos-offset="200" data-aos-delay="50" data-aos-duration="5000" className="uppercase flex font-bold text-2xl md:text-6xl xl:text-7xl">Frontend Developer</h1>
         <div data-aos="flip-up" data-aos-offset="200" data-aos-delay="50" data-aos-duration="5000" className="flex xl:text-md font-medium ">
--- pages/Work.tsx
+++ pages/Work.tsx
@@ -18,47 +18,47 @@ const Work = () => {
 
 
   return (
-    <motion.div  className="flex xl:h-screen ">
+    <motion.div  className="flex xl:h-screen h-[1300px] ">
         <Nav />
         <div className="grid grid-cols-1 h-full  w-screen bg-gray-200 ">
             <MobileNav />
             <motion.div exit={{ scale: 0}} initial={{ scale: 0}} animate={{  x: 0, y: 0, scale: 1, rotate:0,}} transition={{duration:0.9}}  id='rightside' className="grid  movement grid-cols-1 gap-6  xl:gap-40 my-4 ">
-               <div className="grid xl:grid-cols-2 xl:h-[350px] overflow-hidden shadow-2xl mt-4 mx-4 xl:mx-20">
+               <div className="grid xl:grid-cols-2 h-[350px]  overflow-hidden shadow-2xl mt-4 mx-4 xl:mx-20">
                 <div className="flex flex-col pt-2 bg-gray-400 border-gray-200  bg-cover bg-no-repeat backdrop-blur-lg bg-opacity-30 backdrop-opacity-90">
                     <h1 data-aos="flip-up" data-aos-offset="0" data-aos-delay="120" data-aos-duration="9000" className="px-6 uppercase xl:text-5xl text-base xl:px-36 font-bold flex">travel life</h1>
                     <p data-aos="fade" data-aos-offset="0" data-aos-delay="120" data-aos-duration="9000" className="px-6  py-2 xl:px-20 xl:text-lg text-xs xl:py-6 text-gray-700">Travel life is a travel guide website built from scratch by me. the site is built with Next JS, Typescript, Sanity, and style with Tailwind CSS. Each travel destination is render by sanity backend SSR.</p>
                     <div className="flex items-center justify-center mb-4 xl:mb-6 ">
-                        <img src="/next-js.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4" />
-                        <img src="/sanity.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4" />
-                        <img src="/tailwind-css.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4" />
-                        <img src="/typescript-icon.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4" />
+                        <img src="/next-js.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4 md:w-7 md:h-7" />
+                        <img src="/sanity.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4 md:w-7 md:h-7" />
+                        <img src="/tailwind-css.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4 md:w-7 md:h-7" />
+                        <img src="/typescript-icon.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4 md:w-7 md:h-7" />
                     </div>
                     <Link href={'https://travel-life-psi.vercel.app/'} ><button className="w-full p-3 h-full text-white xl:text-3xl  bg-yellow-500 hover:bg-black duration-500 uppercase ">Visit site</button></Link>
                 </div>
                 <Link href={'https://travel-life-psi.vercel.app/'}><img data-aos="flip-up" data-aos-offset="0" data-aos-delay="120" data-aos-duration="9000" src="travel.png" alt="" className="cursor-pointer h-full w-full hover:scale-110 object-cover" /></Link>
                </div>
-               <div className="grid xl:grid-cols-2 xl:h-[350px] shadow-2xl mx-4 xl:mx-20">
+               <div className="grid xl:grid-cols-2 h-[350px] shadow-2xl mx-4 xl:mx-20">
                 <div className="flex flex-col pt-2 bg-gray-400 border-gray-200  bg-cover bg-no-repeat backdrop-blur-lg bg-opacity-30 backdrop-opacity-90">
                     <h1 data-aos="flip-up" data-aos-offset="0" data-aos-delay="120" data-aos-duration="9000" className="px-6 uppercase xl:text-4xl text-base xl:px-36 font-bold flex">Movement Blog</h1>
                     <p data-aos="fade" data-aos-offset="0" data-aos-delay="120" data-aos-duration="9000" className="px-6  py-2 xl:px-20 xl:text-lg text-xs xl:py-6 text-gray-700">Movement is a blog built with Next-js, TypeScript, and style by Tailwind, with Sanity working the back-end Server Side Rendering. Each article is render out using Sanity, that includes titles, pictures, authors, and subject.</p>
                     <div className="flex items-center justify-center mb-4 xl:mb-6 ">
-                        <img src="/next-js.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4" />
-                        <img src="/sanity.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4" />
-                        <img src="/tailwind-css.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4" />
-                        <img src="/typescript-icon.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4" />
+                        <img src="/next-js.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4 md:w-7 md:h-7" />
+                        <img src="/sanity.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4 md:w-7 md:h-7" />
+                        <img src="/tailwind-css.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4 md:w-7 md:h-7" />
+                        <img src="/typescript-icon.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4 md:w-7 md:h-7" />
                     </div>
                     <Link href={'https://nextjs-blog-movement1.vercel.app/'} ><button className="w-full p-3 h-full text-white xl:text-3xl  bg-black hover:bg-blue-500 duration-500 uppercase">Visit site</button></Link>
                 </div>
                 <Link href={'https://nextjs-blog-movement1.vercel.app/'}><img data-aos="flip-up" data-aos-offset="0" data-aos-delay="120" data-aos-duration="9000" src="movement-thumbnail.png" alt="" className="cursor-pointer h-full w-full object-cover " /></Link>
                </div> 
-               <div className="grid xl:grid-cols-2 xl:h-[350px] shadow-2xl mx-4 xl:mx-20">
+               <div className="grid xl:grid-cols-2 h-[350px] shadow-2xl mx-4 xl:mx-20">
                 <div className="flex flex-col bg-gray-400 border-gray-200  bg-cover bg-no-repeat backdrop-blur-lg bg-opacity-30 backdrop-opacity-90 pt-2 ">
                     <h1 data-aos="flip-up" data-aos-offset="0" data-aos-delay="120" data-aos-duration="9000" className="px-6 uppercase xl:text-4xl text-base xl:px-36 font-bold flex">NFT react natvie app</h1>
                     <p data-aos="fade" data-aos-offset="0" data-aos-delay="120" data-aos-duration="9000" className="px-6  py-2 xl:px-20 xl:text-lg text-xs xl:py-6 text-gray-700">This NFT Application was built with React Native along with the Expo Go App. Along with app is a React built Responsive landing page styled by Tailwind CSS.</p>
                     <div className="flex items-center justify-center mb-4 xl:mb-6 ">
-                        <img src="/react.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4" />
-                        <img src="/javascript-js.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4" />
-                        <img src="/tailwind-css.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4" />
+                        <img src="/react.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4 md:w-7 md:h-7" />
+                        <img src="/javascript-js.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4 md:w-7 md:h-7" />
+                        <img src="/tailwind-css.svg" alt="" className="xl:w-8 xl:h-8 mx-2 h-4 w-4 md:w-7 md:h-7" />
                     </div>
                     <Link href={'https://blairnft.netlify.app/'} ><button className="w-full h-full p-3 text-white xl:text-3xl  bg-gradient-to-r from-indigo-500 to to-blue-400 hover:bg-red-400 duration-500 uppercase">Visit site</button></Link>
                 </div>
